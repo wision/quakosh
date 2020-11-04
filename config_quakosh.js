941f8237bb3363c3c68a41093ffb781ca1c4fdac
Tomas Dvorak <mrdeepress@gmail.com>
2020-11-04 15:45:34 +0100

    Revert "Abych zavrel wriesovi hubu"
    This reverts commit 2452f798652ffcecc52916a85a8ed6858e317170.

--- config_quakosh.js
+++ config_quakosh.js
@@ -14,19 +14,42 @@ module.exports = {
 			pass: process.env.QUAKOSH_IRC_AUTH_PASS,
 		},
 		channels: [
-			
+			{
+				name: "#quake.cz",
+				type: "irc",
+				proxy: [
+					{
+						name: "czechquake",
+						type: "discord",
+					},
+				],
+			},
+			{
+				name: "#qw.cz",
+				type: "irc",
+			},
+			{
+				name: "#ibh",
+				type: "irc",
+			},
 		],
 		updateUsers: 60 * 1000,
 		updateName: 60 * 1000,
 		colors: ["02", "03", "04", "05", "06", "07", "09", "10", "11", "12", "13"],
 	},
 	discord: {
-		token: 'NjUyMDg0MjI4MjM3OTUwOTc2.XejUPw.rODvwF7Vq56pClsyQz89w56Wklo',
+		token: process.env.QUAKOSH_DISCORD_TOKEN,
 		guildId: '288985933204946946',
 		channels: [
 			{
-				name: "general",
+				name: "czechquake",
 				type: "discord",
+				proxy: [
+					{
+						name: "#quake.cz",
+						type: "irc",
+					},
+				],
 			},
 		]
 	},
